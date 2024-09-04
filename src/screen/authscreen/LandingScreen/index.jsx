3c62d2dab9e86d4343a1c30e61b7f04d848254c7
Mayur Patel <pmayur392@gmail.com>
2024-09-04 23:58:21 +0530

    design

--- src/screen/authscreen/LandingScreen/index.jsx
+++ src/screen/authscreen/LandingScreen/index.jsx
@@ -4,21 +4,21 @@ import doctorImage from '../../../img/logo.svg';
 import './../../../assest/commoncss.css'
 const LandingScreen = () => {
     return (
-        <div className="bg-primary vh-100 d-flex align-items-center justify-content-center">
+        <div className="vh-100 d-flex align-items-center justify-content-center splashscreen-main">
             <div className="text-center p-3">
                 <div className="mb-5 pb-5">
                     <Link to="landing.html" className="gap-2 text-white text-center">
                         <div className="rounded-pill bg-white p-3 d-inline-block mb-4">
-                            <img src={doctorImage} alt="Logo" className="img-fluid logo" />
+                            <img src={doctorImage} alt="Logo" className="img-fluid logo main-logo" />
                         </div>
-                        <h1 className="mb-0">Totality - The Ultimate Health Solution</h1>
+                        <h1 className="mb-0 splach-title">Totality - The Ultimate Health Solution</h1>
 
                     </Link>
                 </div>
             </div>
 
-            <div className="footer fixed-bottom p-3 footer-custom">
-                <Link to={"/onbordingscreen"} className="btn btn-secondary1 btn-lg w-100">
+            <div className="footer fixed-bottom p-3 footer-custom ">
+                <Link to={"/onbordingscreen"} className="btn splash-btn btn-lg w-100">
                     Click Here
                 </Link>
             </div>
--- src/screen/authscreen/WelcomeScreen/index.jsx
+++ src/screen/authscreen/WelcomeScreen/index.jsx
@@ -1,50 +1,38 @@
-import React, {  } from 'react'
-import { Link,  } from 'react-router-dom';
-import doctorImage from '../../../img/logo.svg';
+import React from "react";
+import { Link } from "react-router-dom";
+import doctorImage from "../../../img/logo.svg";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './../../../assest/commoncss.css'
+import "./../../../assest/commoncss.css";
 const WelcomeScreen = () => {
-    
-    return (
-        <div className="welcome d-flex align-items-center justify-content-center vh-100">
-            <div className="text-center pb-5 mb-5">
-                <div className="mb-5">
-                    <img src={doctorImage} alt="Logo" className="img-fluid blue-logo mb-3" />
-                    <h1 className="text-primary mb-1">Dactorapp</h1>
-                    <p className="text-muted">Welcome to Dactorapp</p>
-                </div>
-                {/* <div className='fixed-bottom'>
-            <div className="footer  p-3 footer-custom">
-                <Link to={"/signup"} className="btn btn-primary btn-lg w-100">
-                Sign Up
-                </Link>
-            </div>
-            <div className="footer  p-3 footer-custom">
-                <Link to={"/login"} className="btn btn-outline-primary btn-lg w-100">
-                
-                </Link>
-            </div>
-            </div> */}
-            <div className='fixed-bottom'>
-    <div className="footer p-3 footer-custom d-flex justify-content-between">
-        <Link to={"/signup"} className="btn btn-primary btn-lg w-100 m-2">
-            Sign Up
-        </Link>
-        <Link to={"/login"} className="btn btn-outline-primary btn-lg w-100 m-2">
-            Log In
-        </Link>
-    </div>
-</div>
-            </div>
-            
-            {/* <div className="footer footer-custom fixed-bottom d-grid gap-3 m-4">
-                <Link to="/sign-up" className="btn btn-primary btn-lg">Sign Up</Link>
-                <Link to="/sign-in" className="btn btn-outline-primary btn-lg">Sign In</Link>
-            </div> */}
-            
+  return (
+    <div className="welcome d-flex align-items-center justify-content-center vh-100">
+      <div className="text-center pb-5 mb-5">
+        <div className="mb-5">
+          <img
+            src={doctorImage}
+            alt="Logo"
+            className="img-fluid blue-logo mb-3"
+          />
+          <h1 className="text-primary mb-1">Dactorapp</h1>
+          <p className="text-muted">Welcome to Dactorapp</p>
         </div>
-    );
+        <div className="fixed-bottom ">
+          <div className="footer p-3 footer-custom d-flex justify-content-between welcome_btn">
+            <Link to={"/signup"} className="btn btn-primary btn-lg w-100 m-2">
+              Sign Up
+            </Link>
+            <Link
+              to={"/login"}
+              className="btn btn-outline-primary btn-lg w-100 m-2"
+            >
+              Log In
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WelcomeScreen;

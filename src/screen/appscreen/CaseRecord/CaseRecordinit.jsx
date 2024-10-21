ccbc26c8cd7102fe8aaee3446383bc914f0c9ad7
bhavikr_2023 <bhavikr@hyperlinkinfosystem.net.in>
2024-10-21 23:53:36 +0530

    commit

--- src/screen/appscreen/CaseRecord/CaseRecordinit.jsx
+++ src/screen/appscreen/CaseRecord/CaseRecordinit.jsx
@@ -3,13 +3,42 @@ import React, { useRef, useState,useEffect } from "react";
 // import Tabs from "react-bootstrap/Tabs";
 import { ChevronRight } from 'react-feather';
 import { Tabs, Tab } from 'react-bootstrap'; 
+
+import { MdArrowDropDown } from "react-icons/md";
 import PatientSection from "./CommonRender/PatientSection";
+
+
+
+
 const CaseRecordinit = () => {
   const [showWeightSections, setShowWeightSections] = useState(false);
+  const [showfecialSections, setshowfecialSections] = useState(false);
+  const [showphysicalDescription, setshowphysicalDescription] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState({});
+  const [physicalDescription, setPhysicalDescription] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [FACIALCONFIGURATATIONEXPRESSION, setFACIALCONFIGURATATIONEXPRESSION] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
   const [WEIGHT, setWEIGHT] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
   const [weightGain, setWeightGain] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
   const [weightLoss, setWeightLoss] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [period, setPeriod] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [height, setHeight] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [facialConfiguration, setFacialConfiguration] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [facialExpression, setFacialExpression] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [chin, setChin] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [lean, setlean] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [stocky, setstocky] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [obese, setobese] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [partial, setpartial] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [general, setgeneral] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [waterlogging, setwaterlogging] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [umbilicusabove, setumbilicusabove] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [below, setbelow] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [emaciation, setemaciation] = useState({ inputValue: '', locationValue: '', rows: [], showComments: true });
+  const [inputValue, setInputValue] = useState("");
+  const [dropdownValue, setDropdownValue] = useState(""); // To store the selected dropdown value
+  const [showComments, setShowComments] = useState(false); // Initial state to show comments
+  const [locationValue, setLocationValue] = useState(null); // Tracks the selected locationValue
+
   const [activeTab, setActiveTab] = useState('Appearance');
   const tabContainerRef = useRef(null);
   const tabRefs = useRef({});
@@ -158,7 +187,202 @@ const CaseRecordinit = () => {
         </div>
         </div>
 
+        <div className="Patient_Person_Add_main">
+      <div className='Patient_Person_main_1'>
+        <PatientSection
+          title="FACIAL CONFIGURATATION & EXPRESSION"
+          section={FACIALCONFIGURATATIONEXPRESSION}
+          setSection={setFACIALCONFIGURATATIONEXPRESSION}
+          dropdownOpen={dropdownOpen}
+          toggleDropdown={toggleDropdown}
+          handleDropdownSelect={handleDropdownSelect}
+          handleAddItem={handleAddItem}
+          handleRemoveItem={handleRemoveItem}
+          toggleCommentsVisibility={toggleCommentsVisibility}
+          ishideshowbutton={true}
+          setShowWeightSections={setshowfecialSections}
+          showWeightSections={showfecialSections}
+        />
+
+{showfecialSections && (
+          <>
+            <PatientSection
+              title="FACIAL CONFIGURATATION"
+              isShowIcon={true}
+              section={facialConfiguration}
+              setSection={setFacialConfiguration}
+              dropdownOpen={dropdownOpen}
+              toggleDropdown={toggleDropdown}
+              handleDropdownSelect={handleDropdownSelect}
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              toggleCommentsVisibility={toggleCommentsVisibility}
+            />
+            <PatientSection
+              title="FACIAL EXPRESSION"
+              isShowIcon={true}
+              section={facialExpression}
+              setSection={setFacialExpression}
+              dropdownOpen={dropdownOpen}
+              toggleDropdown={toggleDropdown}
+              handleDropdownSelect={handleDropdownSelect}
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              toggleCommentsVisibility={toggleCommentsVisibility}
+            />
+          </>
+        )}
+
         
+        </div>
+        </div>
+
+        <div className="Patient_Person_Add_main">
+      <div className='Patient_Person_main_1'>
+        <PatientSection
+          title="PERIOD"
+          section={period}
+          setSection={setPeriod}
+          dropdownOpen={dropdownOpen}
+          toggleDropdown={toggleDropdown}
+          handleDropdownSelect={handleDropdownSelect}
+          handleAddItem={handleAddItem}
+          handleRemoveItem={handleRemoveItem}
+          toggleCommentsVisibility={toggleCommentsVisibility}
+          ishideshowbutton={false}
+          setShowWeightSections={setshowfecialSections}
+          showWeightSections={() => console.log("")
+          }
+        />
+
+
+
+        
+        </div>
+        </div>
+       
+        <div className="Patient_Person_Add_main">
+      <div className='Patient_Person_main_1'>
+        <PatientSection
+          title="PHYSICAL DESCRIPTION"
+          section={physicalDescription}
+          setSection={setPhysicalDescription}
+          dropdownOpen={dropdownOpen}
+          toggleDropdown={toggleDropdown}
+          handleDropdownSelect={handleDropdownSelect}
+          handleAddItem={handleAddItem}
+          handleRemoveItem={handleRemoveItem}
+          toggleCommentsVisibility={toggleCommentsVisibility}
+          ishideshowbutton={true}
+          setShowWeightSections={setshowphysicalDescription}
+          showWeightSections={showphysicalDescription}
+          
+        />
+
+{showphysicalDescription && (
+          <>
+            <PatientSection
+              title="LEAN"
+              isShowIcon={true}
+              section={lean}
+              setSection={setlean}
+              dropdownOpen={dropdownOpen}
+              toggleDropdown={toggleDropdown}
+              handleDropdownSelect={handleDropdownSelect}
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              toggleCommentsVisibility={toggleCommentsVisibility}
+            />
+            <PatientSection
+              title="STOCKY"
+              isShowIcon={true}
+              section={stocky}
+              setSection={setstocky}
+              dropdownOpen={dropdownOpen}
+              toggleDropdown={toggleDropdown}
+              handleDropdownSelect={handleDropdownSelect}
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              toggleCommentsVisibility={toggleCommentsVisibility}
+            />
+            <PatientSection
+              title="OBESE"
+              isShowIcon={true}
+              section={obese}
+              setSection={setobese}
+              dropdownOpen={dropdownOpen}
+              toggleDropdown={toggleDropdown}
+              handleDropdownSelect={handleDropdownSelect}
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              toggleCommentsVisibility={toggleCommentsVisibility}
+            />
+             <PatientSection
+              title="PARTIAL"
+              isShowIcon={true}
+              section={partial}
+              setSection={setpartial}
+              dropdownOpen={dropdownOpen}
+              toggleDropdown={toggleDropdown}
+              handleDropdownSelect={handleDropdownSelect}
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              toggleCommentsVisibility={toggleCommentsVisibility}
+            />
+             <PatientSection
+              title="GENERAL"
+              isShowIcon={true}
+              section={general}
+              setSection={setgeneral}
+              dropdownOpen={dropdownOpen}
+              toggleDropdown={toggleDropdown}
+              handleDropdownSelect={handleDropdownSelect}
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              toggleCommentsVisibility={toggleCommentsVisibility}
+            />
+             <PatientSection
+              title="WATER-LOGGING"
+              isShowIcon={true}
+              section={waterlogging}
+              setSection={setwaterlogging}
+              dropdownOpen={dropdownOpen}
+              toggleDropdown={toggleDropdown}
+              handleDropdownSelect={handleDropdownSelect}
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              toggleCommentsVisibility={toggleCommentsVisibility}
+            />
+            <PatientSection
+              title="WATER-LOGGING"
+              isShowIcon={true}
+              section={waterlogging}
+              setSection={setwaterlogging}
+              dropdownOpen={dropdownOpen}
+              toggleDropdown={toggleDropdown}
+              handleDropdownSelect={handleDropdownSelect}
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              toggleCommentsVisibility={toggleCommentsVisibility}
+            />
+             <PatientSection
+              title="WATER-LOGGING"
+              isShowIcon={true}
+              section={waterlogging}
+              setSection={setwaterlogging}
+              dropdownOpen={dropdownOpen}
+              toggleDropdown={toggleDropdown}
+              handleDropdownSelect={handleDropdownSelect}
+              handleAddItem={handleAddItem}
+              handleRemoveItem={handleRemoveItem}
+              toggleCommentsVisibility={toggleCommentsVisibility}
+            />
+          </>
+        )}
+
+        
+        </div>
+        </div>
        
       </>
     </Tab>
